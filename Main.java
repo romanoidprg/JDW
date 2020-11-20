f87d3a57175c1b4baac016e3f38b296663357848
romanoidprg <70508104+romanoidprg@users.noreply.github.com>
2020-11-20 23:10:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.epam.jwd.app;
+
+import com.epam.jwd.model.Point;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Logger logger = LogManager.getLogger(Main.class);
+
+        Point[] pointArray = new Point[5];
+
+        for (int i = 0; i<5; i++){
+            pointArray[i] = new Point(i,i*2);
+        }
+
+        for (int i = 0; i<5; i++){
+            logger.debug(pointArray[i].toString());
+        }
+    }
+}
